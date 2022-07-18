9501992ac301b680995a7bced764d0b08c8ca797
Harshit <absinghal10@gmail.com>
2022-07-18 21:46:12 +0530

    Update branches

---
+++ app/src/main/java/com/abhishek/hellowordapplication/HelloworldActivity.java
@@ -0,0 +1,14 @@
+package com.abhishek.hellowordapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelloworldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helloworld);
+    }
+}
